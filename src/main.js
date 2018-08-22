d5cab14e26fa993317d8f117102cdeaa4031a69f
Anoop P Oommen <allenshaji10@gmail.com>
2018-08-22 11:14:18 +0530

    corrections

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -9,13 +7,12 @@ import "bootstrap-vue/dist/bootstrap-vue.css"
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
-
-
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(VueAxios, axios);
 window.axios = require('axios');
-/* eslint-disable no-new */
+
+
 new Vue({
   el: '#app',
   router,
